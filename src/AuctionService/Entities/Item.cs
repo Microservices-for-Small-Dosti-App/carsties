ccf93661394ee3818866cb37d0e991a7cbbbeb99
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-18 18:20:23 +0530

    Update Item.cs

--- src/AuctionService/Entities/Item.cs
+++ src/AuctionService/Entities/Item.cs
@@ -7,21 +7,21 @@ namespace AuctionService;
 public class Item
 {
     public Guid Id { get; set; }
-    
+
     public string Make { get; set; }
-    
+
     public string Model { get; set; }
-    
+
     public int Year { get; set; }
-    
+
     public string Color { get; set; }
-    
+
     public int Mileage { get; set; }
-    
+
     public string ImageUrl { get; set; }
 
-    // nav properties
-    public Auction Auction { get; set; }
-    
+    // navigation properties
     public Guid AuctionId { get; set; }
+
+    public Auction Auction { get; set; }
 }
