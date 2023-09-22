febadd8f54f1834ba9c81fc43096ddc2ecc69d25
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-22 13:47:36 +0530

    Update Program.cs

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddDbContext<AuctionDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
