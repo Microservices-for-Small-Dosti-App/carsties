de2f79d5805e7cf40597509cfe4b727c9b31df9f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-29 17:15:47 +0530

    Searching

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -15,11 +15,38 @@ public class SearchController : ControllerBase
     {
         var query = DB.PagedSearch<Item, Item>();
 
-        query.Sort(x => x.Ascending(a => a.Make));
-
         if (!string.IsNullOrEmpty(searchParams.SearchTerm))
         {
             query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
+        }
+
+        query = searchParams.OrderBy switch
+        {
+            "make" => query.Sort(x => x.Ascending(a => a.Make))
+                .Sort(x => x.Ascending(a => a.Model)),
+            "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
+            _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
+        };
+
+        if (!string.IsNullOrEmpty(searchParams.FilterBy))
+        {
+            query = searchParams.FilterBy switch
+            {
+                "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+                "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6)
+                    && x.AuctionEnd > DateTime.UtcNow),
+                _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
+            };
+        }
+
+        if (!string.IsNullOrEmpty(searchParams.Seller))
+        {
+            query.Match(x => x.Seller == searchParams.Seller);
+        }
+
+        if (!string.IsNullOrEmpty(searchParams.Winner))
+        {
+            query.Match(x => x.Winner == searchParams.Winner);
         }
 
         query.PageNumber(searchParams.PageNumber);
@@ -34,31 +61,6 @@ public class SearchController : ControllerBase
             totalCount = result.TotalCount
         });
 
-        // query = searchParams.OrderBy switch
-        // {
-        //     "make" => query.Sort(x => x.Ascending(a => a.Make))
-        //         .Sort(x => x.Ascending(a => a.Model)),
-        //     "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
-        //     _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
-        // };
-
-        // query = searchParams.FilterBy switch
-        // {
-        //     "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
-        //     "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6)
-        //         && x.AuctionEnd > DateTime.UtcNow),
-        //     _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
-        // };
-
-        // if (!string.IsNullOrEmpty(searchParams.Seller))
-        // {
-        //     query.Match(x => x.Seller == searchParams.Seller);
-        // }
-
-        // if (!string.IsNullOrEmpty(searchParams.Winner))
-        // {
-        //     query.Match(x => x.Winner == searchParams.Winner);
-        // }
     }
 
 }
