8a3f407096fd18e7cb7dee477d7c50ae1bc3b103
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-30 20:33:26 +0530

    Http Client

--- src/SearchService/Services/AuctionSvcHttpClient.cs
+++ src/SearchService/Services/AuctionSvcHttpClient.cs
@@ -10,7 +10,7 @@ public class AuctionSvcHttpClient(HttpClient httpClient, IConfiguration config)
 
     public async Task<List<Item>> GetItemsForSearchDb()
     {
-        var lastUpdated = await DB.Find<Item, string>()
+        string? lastUpdated = await DB.Find<Item, string>()
             .Sort(x => x.Descending(x => x.UpdatedAt))
             .Project(x => x.UpdatedAt.ToString())
             .ExecuteFirstAsync();
