5d997d818efed2c89efe585500fb759b6b2e7fd6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-01 16:53:33 +0530

    AddPolicyHandler

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,15 +1,15 @@
-using System.Net;
 using Polly;
 using Polly.Extensions.Http;
 using SearchService;
 using SearchService.Data;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 
-builder.Services.AddHttpClient<AuctionSvcHttpClient>();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
