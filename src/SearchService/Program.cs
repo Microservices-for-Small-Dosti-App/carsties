8290ab5eeffe7cbefc70f8c567f2b45fbed7d5ad
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-06 17:32:02 +0530

    Update Program.cs

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,6 +1,7 @@
 using MassTransit;
 using Polly;
 using Polly.Extensions.Http;
+using SearchService.Consumers;
 using SearchService.Data;
 using SearchService.Services;
 using System.Net;
@@ -16,6 +17,10 @@ builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolic
 
 builder.Services.AddMassTransit(x =>
 {
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+    
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
+    
     x.UsingRabbitMq((context, cfg) =>
     {
         cfg.ConfigureEndpoints(context);
