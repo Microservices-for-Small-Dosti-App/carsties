a8d3ab0ade1a07bcfb44ba00e90741fd86464949
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-08 16:09:58 +0530

    Changes

---
+++ src/IdentityService/Pages/Account/Register/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityService;
+
+public class RegisterViewModel
+{
+    public required string Email { get; set; }
+
+    public required string Password { get; set; }
+
+    public required string Username { get; set; }
+
+    public required string FullName { get; set; }
+
+    public string? ReturnUrl { get; set; }
+
+    public string? Button { get; set; }
+}
