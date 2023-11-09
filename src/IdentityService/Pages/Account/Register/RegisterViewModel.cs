2c5952c23f3007035e17dcde4d430fdd5885c43c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-09 09:22:58 +0530

    Swamy/29oct microservices day17 (#21)
    * Update HostingExtensions.cs
    
    * Packages
    
    * Update SeedData.cs
    
    * Update SeedData.cs
    
    * Update SeedData.cs
    
    * Update SeedData.cs
    
    * Seed Data Changes
    
    * Removed the migrations folder
    
    * Seed Data Changes
    
    * EF Migrations
    
    * Update 20231029151036_InitialCreate.cs
    
    * Update HostingExtensions.cs
    
    * Register
    
    * Update Index.cshtml
    
    * Changes
    
    * Change

---
+++ src/IdentityService/Pages/Account/Register/RegisterViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace IdentityService;
+
+public class RegisterViewModel
+{
+    public required string Email { get; set; }
+
+    public required string Password { get; set; }
+
+    public required string Username { get; set; }
+
+    public required string FullName { get; set; }
+
+    public string? ReturnUrl { get; set; }
+
+    public string? Button { get; set; }
+}
