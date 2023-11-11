d162da408a0ff39120f037e7fdc8526e3c049889
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-11 19:53:41 +0530

    Update Config.cs

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -20,19 +20,21 @@ public static class Config
     public static IEnumerable<Client> Clients =>
         new Client[]
         {
-            // interactive client using code flow + pkce
+            // For Postman (ONLY FOR DEVELOPMENT !!)
             new() {
-                ClientId = "interactive",
+                ClientId = "postman",
+                ClientName = "Postman",
+                AllowedScopes = { "openid", "profile", "auctionApp" },
+                RedirectUris = { "https://localhost:44300/signin-oidc" },
                 ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
 
                 AllowedGrantTypes = GrantTypes.Code,
 
-                RedirectUris = { "https://localhost:44300/signin-oidc" },
                 FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
                 PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
 
                 AllowOfflineAccess = true,
-                AllowedScopes = { "openid", "profile", "scope2" }
+
             },
         };
 }
