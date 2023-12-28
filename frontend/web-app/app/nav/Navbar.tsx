f5af2e4fd281d4992d4142b5daa1c23b023b0140
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-28 13:13:07 +0530

    Navbar

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Navbar() {
     return (
-        <header>
+        <header className='sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md'>
             <div>Left</div>
             <div>Middle</div>
             <div>Right</div>
