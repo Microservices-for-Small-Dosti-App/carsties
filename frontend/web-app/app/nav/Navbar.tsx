1e4f4962c203781fca519f056a1a9ae15a76a878
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-28 13:00:28 +0530

    Create Navbar.tsx

---
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <header>
+            <div>Left</div>
+            <div>Middle</div>
+            <div>Right</div>
+        </header>
+    );
+}
