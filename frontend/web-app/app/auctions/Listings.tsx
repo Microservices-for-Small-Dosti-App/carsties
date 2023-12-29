57e187b07db535486b5b964454af5fc0e1a0adb0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-29 22:45:37 +0530

    Codeing

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import AuctionCard from './AuctionCard';
 
 async function getData() {
-    const response = await fetch('http://localhost:6001/search');
+    const response = await fetch('http://localhost:6001/search?pageSize=10');
 
     if (!response.ok) throw new Error('Failed to fetch data');
 
