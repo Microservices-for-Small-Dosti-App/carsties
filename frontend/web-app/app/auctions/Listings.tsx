81d4b1de612819403614394b29c92c0c1d60ad2b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-02 13:20:39 +0530

    Update Listings.tsx

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import AuctionCard from './AuctionCard';
 import { Auction, PagedResult } from '@/types';
+import AppPagination from '../components/AppPagination';
 
 async function getData(): Promise<PagedResult<Auction>> {
-    const response = await fetch('http://localhost:6001/search?pageSize=10');
+    const response = await fetch('http://localhost:6001/search?pageSize=4');
 
     if (!response.ok) throw new Error('Failed to fetch data');
 
@@ -15,11 +16,16 @@ export default async function Listings() {
     const data: PagedResult<Auction> = await getData();
 
     return (
-        <div className='grid grid-cols-4 gap-6'>
-            {data && data.results.map((auction: Auction) => (
-                <AuctionCard key={auction.id} auction={auction} />
-            ))}
-        </div>
+        <>
+            <div className='grid grid-cols-4 gap-6'>
+                {data && data.results.map((auction: Auction) => (
+                    <AuctionCard key={auction.id} auction={auction} />
+                ))}
+            </div>
+            <div className='flex justify-center mt-4'>
+                <AppPagination currentPage={1} pageCount={data.pageCount} />
+            </div>
+        </>
     );
 }
 
