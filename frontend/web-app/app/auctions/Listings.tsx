c762529f0c6481e084da1da59104fb84d4b719dc
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-02 13:50:30 +0530

    Server Action

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,23 +1,38 @@
-import React from 'react'
+'use client';
+
+import { useEffect, useState } from 'react';
 import AuctionCard from './AuctionCard';
-import { Auction, PagedResult } from '@/types';
+import { Auction } from '@/types';
 import AppPagination from '../components/AppPagination';
-
-
+import { getData } from '../actions/auctionActions';
 
 export default function Listings() {
 
-    const data: PagedResult<Auction> = await getData();
+    const [auctions, setAuctions] = useState<Auction[]>([]);
+    const [pageCount, setPageCount] = useState<number>(0);
+    const [pageNumber, setPageNumber] = useState<number>(1);
+
+    useEffect(() => {
+        getData(pageNumber)
+            .then((data: any) => {
+                setAuctions(data.results);
+                setPageCount(data.pageCount);
+            })
+    }, [pageNumber]);
+
+    if (auctions.length === 0) {
+        return <div>Loading...</div>
+    }
 
     return (
         <>
             <div className='grid grid-cols-4 gap-6'>
-                {data && data.results.map((auction: Auction) => (
+                {auctions.map((auction: Auction) => (
                     <AuctionCard key={auction.id} auction={auction} />
                 ))}
             </div>
             <div className='flex justify-center mt-4'>
-                <AppPagination currentPage={1} pageCount={data.pageCount} />
+                <AppPagination currentPage={pageNumber} pageCount={pageCount} />
             </div>
         </>
     );
