0979341f9fbb04eab8bff381f5c04fa39f9daf72
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-04 07:10:30 +0530

    Swamy/03jan microservices day28 (#36)
    * Create Filters.tsx
    
    * Page Size Filter
    
    * Update next.config.js

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -5,20 +5,22 @@ import AuctionCard from './AuctionCard';
 import { Auction } from '@/types';
 import AppPagination from '../components/AppPagination';
 import { getData } from '../actions/auctionActions';
+import Filters from './Filters';
 
 export default function Listings() {
 
     const [auctions, setAuctions] = useState<Auction[]>([]);
     const [pageCount, setPageCount] = useState<number>(0);
     const [pageNumber, setPageNumber] = useState<number>(1);
+    const [pageSize, setPageSize] = useState<number>(4);
 
     useEffect(() => {
-        getData(pageNumber)
+        getData(pageNumber, pageSize)
             .then((data: any) => {
                 setAuctions(data.results);
                 setPageCount(data.pageCount);
             })
-    }, [pageNumber]);
+    }, [pageNumber, pageSize]);
 
     if (auctions.length === 0) {
         return <div>Loading...</div>
@@ -26,6 +28,8 @@ export default function Listings() {
 
     return (
         <>
+            <Filters pageSize={pageSize} setPageSize={setPageSize} />
+            
             <div className='grid grid-cols-4 gap-6'>
                 {auctions.map((auction: Auction) => (
                     <AuctionCard key={auction.id} auction={auction} />
--- frontend/web-app/next.config.js
+++ frontend/web-app/next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost', 'cdn.pixabay.com'],
+        // domains: ['localhost', 'cdn.pixabay.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'cdn.pixabay.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '',
+                pathname: '/**',
+            },
+        ],
     },
 }
 
