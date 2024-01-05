e03da535e4615b7fb908d0267fdaac43e583d3ea
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-05 10:17:54 +0530

    Update useParamsStore.ts

--- frontend/web-app/hooks/useParamsStore.ts
+++ frontend/web-app/hooks/useParamsStore.ts
@@ -1,32 +1,50 @@
 import { create } from "zustand"
 
 type State = {
-    pageNumber: number
-    pageSize: number
-    pageCount: number
-    searchTerm: string
-    searchValue: string
-    orderBy: string
-    filterBy: string
-    seller?: string
-    winner?: string
+    pageNumber: number;
+
+    pageSize: number;
+
+    pageCount: number;
+
+    searchTerm: string;
+
+    searchValue: string;
+
+    orderBy: string;
+
+    filterBy: string;
+
+    seller?: string;
+
+    winner?: string;
 }
 
 type Actions = {
-    setParams: (params: Partial<State>) => void
-    reset: () => void
-    setSearchValue: (value: string) => void
+    setParams: (params: Partial<State>) => void;
+
+    reset: () => void;
+
+    setSearchValue: (value: string) => void;
 }
 
 const initialState: State = {
     pageNumber: 1,
+
     pageSize: 12,
+    
     pageCount: 1,
+    
     searchTerm: '',
+    
     searchValue: '',
+    
     orderBy: 'make',
+    
     filterBy: 'live',
+    
     seller: undefined,
+    
     winner: undefined
 }
 
@@ -48,4 +66,4 @@ export const useParamsStore = create<State & Actions>()((set) => ({
     setSearchValue: (value: string) => {
         set({ searchValue: value })
     }
-}))
+}));
