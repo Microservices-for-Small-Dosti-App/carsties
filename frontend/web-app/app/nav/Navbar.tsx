5d01b9d6610558b36995b833d7dd498618b590f3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-06 14:37:25 +0530

    Update Navbar.tsx

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -4,7 +4,7 @@ import Search from './Search';
 
 export default function Navbar() {
     return (
-        <header className='sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md'>
+        <header className='sticky top-0 z-50 flex justify-between bg-white-100 p-5 items-center text-gray-800 shadow-md'>
             <div className='flex items-center gap-2 text-3xl font-semibold text-blue-500'>
                 <AiOutlineCar size={34} />
                 <div>Carsties Auctions</div>
