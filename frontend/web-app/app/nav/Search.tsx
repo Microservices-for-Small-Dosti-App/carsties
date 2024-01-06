0f04f4b4239d337269878e679e8637049a2837e7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-06 14:34:03 +0530

    Update Search.tsx

--- frontend/web-app/app/nav/Search.tsx
+++ frontend/web-app/app/nav/Search.tsx
@@ -24,12 +24,10 @@ export default function Search() {
     return (
         <div className='flex w-[50%] items-center border-2 rounded-full py-2 shadow-sm'>
             <input
-                onKeyDown={(e: any) => {
-                    if (e.key === 'Enter') search();
-                }}
+                onKeyDown={(e: any) => { if (e.key === 'Enter') search(); }}
                 value={searchValue} onChange={onChange} type="text"
                 placeholder='Search for cars by make, model or color'
-                className='input-custom text-sm text-gray-600' />
+                className='flex-grow pl-5 bg-transparent focus:outline-none border-transparent focus:border-transparent focus:ring-0 text-sm text-gray-600' />
             <button onClick={search}>
                 <FaSearch size={34} className='bg-blue-500 text-white rounded-full p-2 cursor-pointer mx-2' />
             </button>
