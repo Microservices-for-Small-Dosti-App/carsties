88e568378698a021c56745a2102a642b0ddb2b1e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-06 14:54:18 +0530

    Search working

--- frontend/web-app/app/nav/Search.tsx
+++ frontend/web-app/app/nav/Search.tsx
@@ -24,7 +24,9 @@ export default function Search() {
     return (
         <div className='flex w-[50%] items-center border-2 rounded-full py-2 shadow-sm'>
             <input
-                onKeyDown={(e: any) => { if (e.key === 'Enter') search(); }}
+                onKeyDown={(e: any) => {
+                    if (e.key === 'Enter') search();
+                }}
                 value={searchValue} onChange={onChange} type="text"
                 placeholder='Search for cars by make, model or color'
                 className='flex-grow pl-5 bg-transparent focus:outline-none border-transparent focus:border-transparent focus:ring-0 text-sm text-gray-600' />
