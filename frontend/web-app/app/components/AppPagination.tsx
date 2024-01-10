e9114106600394cd09b7419108f63705e53a4fcd
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-10 08:13:40 +0530

    Swamy/08jan microservices day31 (#39)
    * Next Auth
    
    * Auth Route
    
    * Update route.ts
    
    * Added Login Button
    
    * Create authActions.ts
    
    * Create page.tsx
    
    * User Session
    
    * Update UserActions.tsx
    
    * Profile, and Session updates
    
    * Update Navbar.tsx
    
    * Update UserActions.tsx
    
    * Update UserActions.tsx
    
    * Update AppPagination.tsx

--- frontend/web-app/app/components/AppPagination.tsx
+++ frontend/web-app/app/components/AppPagination.tsx
@@ -11,13 +11,7 @@ type Props = {
 
 export default function AppPagination({ currentPage, pageCount, pageChanged }: Props) {
     return (
-        <Pagination
-            currentPage={currentPage}
-            onPageChange={e => pageChanged(e)}
-            totalPages={pageCount}
-            layout='pagination'
-            showIcons={true}
-            className='text-blue-500 mb-5'
-        />
+            <Pagination currentPage={currentPage} onPageChange={e => pageChanged(e)} className='text-blue-500 mb-5'
+                totalPages={pageCount} layout='pagination' showIcons={true} />
     )
 }
