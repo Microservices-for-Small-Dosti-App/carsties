af5a7dbc58e6441722b66689c282401aed0f2848
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-11 17:23:16 +0530

    Update auctionActions.ts

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { Auction, PagedResult } from "@/types";
+import { getTokenWorkaround } from "./authActions";
 
 export async function getData(query: string): Promise<PagedResult<Auction>> {
     const response = await fetch(`http://localhost:6001/search${query}`);
@@ -15,13 +16,18 @@ export async function updateAuctionTest() {
         mileage: Math.floor(Math.random() * 100000) + 1
     };
 
+    const token = await getTokenWorkaround();
+
     const response = await fetch(`http://localhost:6001/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c`, {
         method: 'PUT',
-        headers: {},
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token?.access_token}`
+        },
         body: JSON.stringify(data)
     });
 
-    if (!response.ok) return {status: response.status, message: response.statusText};
+    if (!response.ok) return { status: response.status, message: response.statusText };
 
-    return response.json();
+    return response.statusText;
 }
