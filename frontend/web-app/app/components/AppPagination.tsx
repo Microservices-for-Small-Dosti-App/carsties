6c9ddd15daffc300630e611f5fa002f6e3ea10a1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-11 15:21:05 +0530

    Update AppPagination.tsx

--- frontend/web-app/app/components/AppPagination.tsx
+++ frontend/web-app/app/components/AppPagination.tsx
@@ -11,7 +11,9 @@ type Props = {
 
 export default function AppPagination({ currentPage, pageCount, pageChanged }: Props) {
     return (
+        <div className="flex overflow-x-auto sm:justify-center">
             <Pagination currentPage={currentPage} onPageChange={e => pageChanged(e)} className='text-blue-500 mb-5'
-                totalPages={pageCount} layout='pagination' showIcons={true} />
+                totalPages={pageCount} layout='navigation' showIcons={true} />
+        </div>
     )
 }
