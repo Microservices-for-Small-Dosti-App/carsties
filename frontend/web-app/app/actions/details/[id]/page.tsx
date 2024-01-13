c16b2e8fe0dd2119f184859e9600392d1d7a0733
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-13 19:36:35 +0530

    Initial Version

--- frontend/web-app/app/actions/details/[id]/page.tsx
+++ frontend/web-app/app/actions/details/[id]/page.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from 'react';
 
 export default function Details({ params }: { params: { id: string } }) {
     return (
-        <div>Details</div>
-    )
-}
+        <div>Details for {params.id}</div>
+    );
+
+};
