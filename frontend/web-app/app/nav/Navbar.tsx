2c5fba62019361e9cbc512213b117def38cb3e73
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-13 19:54:25 +0530

    Update Navbar.tsx

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -9,7 +9,7 @@ export default async function Navbar() {
     const user = await getCurrentUser();
 
     return (
-        <header className='sticky top-0 z-50 flex justify-between bg-white p-3 items-center text-gray-800 shadow-md'>
+        <header className='sticky top-0 z-50 flex justify-between bg-white p-3 items-center text-gray-800 shadow-md shadow-gray-400'>
             <Logo />
             <Search />
             {user ? (
