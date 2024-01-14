85ed9eb071263b9f566edb36b131a04e27031ac1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-14 23:08:43 +0530

    Update DateInput.tsx

--- frontend/web-app/app/components/DateInput.tsx
+++ frontend/web-app/app/components/DateInput.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { UseControllerProps, useController } from 'react-hook-form'
 import DatePicker, { ReactDatePickerProps } from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 type Props = {
     label: string
