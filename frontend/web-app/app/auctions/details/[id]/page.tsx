9d20e553f1ce2d51fe0ce0e4bf61c0afd7d07e67
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-18 23:43:37 +0530

    Update page.tsx

--- frontend/web-app/app/auctions/details/[id]/page.tsx
+++ frontend/web-app/app/auctions/details/[id]/page.tsx
@@ -1,8 +1,34 @@
+import { getDetailedViewData } from '@/app/actions/auctionActions';
+import { getCurrentUser } from '@/app/actions/authActions';
+import Heading from '@/app/components/Heading';
 import React from 'react';
+import CountdownTimer from '../../CountdownTimer';
 
-export default function Details({ params }: { params: { id: string } }) {
+export default async function Details({ params }: { params: { id: string } }) {
+    const data = await getDetailedViewData(params.id);
+    const user = await getCurrentUser();
+
     return (
-        <div>Details for {params.id}</div>
+        <div>
+
+            <div className='flex justify-between'>
+                <div className='flex items-center gap-3'>
+                    <Heading title={`${data.make} ${data.model}`} />
+                    {user?.username === data.seller && (
+                        <>
+                            {/* <EditButton id={data.id} />
+                            <DeleteButton id={data.id} /> */}
+                        </>
+
+                    )}
+                </div>
+
+                <div className='flex gap-3'>
+                    <h3 className='text-2xl font-semibold'>Time remaining:</h3>
+                    <CountdownTimer auctionEnd={data.auctionEnd} />
+                </div>
+            </div>
+        </div>
     );
 
 };
